3b36cd85a3c3252bd461e885c94da24fd2eebbeb
Max Lange <max@langes.us>
2023-03-07 12:28:40 -0500

    Update Api call to be used to post

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,20 @@
-export const getUrls = () => {
-  return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
+export const getUrls = (data) => {
+  if(!data) {
+    return fetch('http://localhost:3001/api/v1/urls')
+      .then(response => {
+        if(response.ok) {
+          return response.json()
+        } else {
+          throw new Error('Something went wrong!')
+        }
+      })
+  } else {
+    return fetch('http://localhost:3001/api/v1/urls', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': "application/json"
+      }
+    })
+  }
 }
