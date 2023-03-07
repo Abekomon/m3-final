11999afce00b1118f3b8a7bfe443f5c584074cb4
Max Lange <max@langes.us>
2023-03-07 14:00:51 -0500

    refactor Api calls to be clearer in functionality

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,20 +1,21 @@
 export const getUrls = (data) => {
-  if(!data) {
-    return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => {
-        if(response.ok) {
-          return response.json()
-        } else {
-          throw new Error('Something went wrong!')
-        }
-      })
-  } else {
-    return fetch('http://localhost:3001/api/v1/urls', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': "application/json"
+  return fetch('http://localhost:3001/api/v1/urls')
+    .then(response => {
+      if(response.ok) {
+        return response.json()
+      } else {
+        throw new Error('Something went wrong!')
       }
     })
-  }
+}
+
+export const postUrls = (data) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': "application/json"
+    }
+  })
+  .then(response => response.json())
 }
